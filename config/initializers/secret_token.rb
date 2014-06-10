80b05970722c278ac1e28e6e4fe900548767f82c
dwightgnjohnson <dwightgnjohnson@gmail.com>
2014-06-10 11:23:29 -0700

    Second try of Movie app from Dave Jones Lecture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lecture2::Application.config.secret_key_base = 'c3ea470df7fa76d1c075a4e5198ffb1066b5b3e403e0ec5ceb8b32a1f0b08b6e13a2075914834d7e26d127c2c4634f3d8fea9fcb72aa3e9d5c8e037f530f3cd2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lecture2::Application.config.session_store :cookie_store, key: '_lecture2_session'
